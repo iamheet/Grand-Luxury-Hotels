73607c6a5296178e444577e3d9f7c5bd21fbb98d
Heet <iamheetchokshi@gmail.com>
2026-04-04 16:26:24 +0530

    Fix emailService import path in routes/email.js

--- routes/email.js
+++ routes/email.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { sendBookingConfirmationEmail, sendCustomEmail, sendWhatsAppBookingConfirmation } = require('../emailService');
+const { sendBookingConfirmationEmail, sendCustomEmail, sendWhatsAppBookingConfirmation } = require('./emailService');
 
 // Send booking confirmation email
 router.post('/booking-confirmation', async (req, res) => {
