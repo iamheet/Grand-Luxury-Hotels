5762b122f2e78a26712f7b03873214a99846a047
Heet <iamheetchokshi@gmail.com>
2026-04-16 17:07:02 +0530

    ty

--- src/pages/Membership.tsx
+++ src/pages/Membership.tsx
@@ -76,7 +76,7 @@ export default function Membership() {
             Each tier unlocks extraordinary benefits and personalized services.
           </p>
         
-          </button>
+    
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-8">
